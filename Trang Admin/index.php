8861a04bf9bf2131e45f8a74944dbe47f17a3803
Du Cuong <ducuong133@gmail.com>
2025-12-14 15:56:31 +0700

    feature(qlloaiphim): Them giao dien xem ds loaiphim

--- Trang Admin/index.php
+++ Trang Admin/index.php
@@ -11,12 +11,35 @@ if (isset($_SESSION['user1'])) {
         exit;
     }
 
+    // 3. Include Models
+    include "./model/pdo.php";
+    include "./model/loai_phim.php";
 
+    // 4. Load Global Data
+    $loadloai = loadall_loaiphim();
 
     // 5. Include Header (Lúc này mới bắt đầu xuất HTML)
     include "./view/home/header.php";
 
-
+    // 6. Điều hướng Controller
+    if (isset($_GET['act']) && ($_GET['act'] != "")) {
+        $act = $_GET['act'];
+        switch ($act) {
+            // Module Loại Phim
+            case "QLloaiphim":
+            case "themloai":
+            case "sualoai":
+            case "xoaloai":
+            case "updateloai":
+                include "./controllers/c_loaiphim.php";
+                break;
+            default:
+                include "./view/home.php";
+                break;
+            }
+    } else {
+        include "./view/home.php";
+    }
 
     include "./view/home/footer.php";
 } else {
